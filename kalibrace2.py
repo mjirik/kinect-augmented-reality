4a0968522c698d96290cd6fb2ddee47eaa713a78
michal_kubat <majkon25@gmail.com>
2013-04-25 14:37:21 +0200

    přidána nová kalibrace ze 14 bodů

--- kalibrace2.py
+++ kalibrace2.py
@@ -1,20 +1,27 @@
+# -*- coding: cp1250 -*-
+
 '''
 Created on 14.3.2013
 
 @author: misa
 '''
+
 import pickle
 import numpy as np
-import sys, pygame
+import os, sys, pygame
 import autobahn
 import twisted
 import random
+from pygame.locals import *
 from twisted.internet import reactor
 from autobahn.websocket import WebSocketClientFactory, \
                                WebSocketClientProtocol, \
                                connectWS
 import json
 
+
+#Window_height = 800
+#Window_width = 1500
 Window_height = 500
 Window_width = 1000
 LOOP_TIME = 0.1
@@ -136,28 +143,79 @@ class EchoClientProtocol(WebSocketClientProtocol):
         pygame.display.update()           
         
         
-    def kalibrace_init(self, new_kalib = True):
+    def kalibrace_init(self, new_kalib = False):
         pygame.init()
         pygame.display.set_caption('Vykresleni bodu')
         self.size = self.width, self.height = Window_width,Window_height
         self.screen=pygame.display.set_mode((self.size),0,32)
         self.background=pygame.image.load(bg).convert()
         self.point=pygame.image.load(bod1).convert_alpha()
-       
+        if pygame.font:
+            pismo = pygame.font.Font(None, 28)
+            text = pismo.render(u"Lovou ruku pøiložte na zobrazovaný bod a pravou zvednìte nad hlavu",1, (10, 10, 10))
+            textPozice = text.get_rect()
+            textPozice.centerx = self.screen.get_rect().centerx
+            self.screen.blit(text, textPozice)
+            
+            print textPozice
+            print textPozice.centerx
+            print text
+            
         self.povoleno = True
-        self.nPoints = 4
+        
+        
+        
+        if new_kalib:
+            self.nPoints = 14
+            self.kalibPtsX = [
+                              50, 
+                              Window_width - 50,
+                              Window_width - 50,
+                              80,
+                              90,
+                              Window_width - 80,
+                              Window_width - 70,
+                              60,
+                              Window_width - 50,
+                              80,
+                              90,
+                              Window_width - 80,
+                              Window_width - 70,
+                              60,
+                              1
+                              ]
+            self.kalibPtsY = [
+                              50,
+                              50,
+                              Window_height - 50,
+                              Window_height - 50,
+                              80,
+                              50,
+                              Window_height - 80,
+                              Window_height - 50,
+                              70,
+                              80,
+                              Window_height - 50,
+                              Window_height - 90,
+                              90,
+                              40,
+                              1
+                              ]
+            self.bod(x = self.kalibPtsX[0], y = self.kalibPtsY[0])
+        else:
+            self.nPoints = 4
+            self.kalibPtsX = [50, Window_width - 50,  Window_width - 50, 50, 1]
+            self.kalibPtsY = [50, 50, Window_height - 50, Window_height - 50, 1]
+            self.bod(x = self.kalibPtsX[0], y = self.kalibPtsY[0])
+        
         self.actualPointInd = 0
         self.projPts = [0]*self.nPoints
         self.kinectPts = [0]*self.nPoints
         
-        self.kalibPtsX = [50, Window_width - 50,  Window_width - 50, 50, 1]
-        self.kalibPtsY = [50, 50, Window_height - 50, Window_height - 50, 1]
-        self.bod(x = self.kalibPtsX[0], y = self.kalibPtsY[0])
-        
+        self.new_kalib = new_kalib
         pass
     
     def kalibrace_run(self):
-        
         head = self.body["Head"]
         rhand = self.body["RightHand"]
         lhand = self.body["LeftHand"]
@@ -167,9 +225,10 @@ class EchoClientProtocol(WebSocketClientProtocol):
         if int(rhand["Y"]) > int(head["Y"]) and self.povoleno:
             x1 = int(lhand["X"])
             y1 = int(lhand["Y"])
+            z1 = int(lhand["Z"])
             
             self.projPts[self.actualPointInd] = [self.px, self.py]
-            self.kinectPts[self.actualPointInd] = [x1, y1]
+            self.kinectPts[self.actualPointInd] = [x1, y1, z1]
             self.actualPointInd = self.actualPointInd + 1
             self.povoleno = False
             print "x1 =",  x1
@@ -182,25 +241,82 @@ class EchoClientProtocol(WebSocketClientProtocol):
         print self.kinectPts
         print self.actualPointInd
         print self.povoleno
+        
+        if self.new_kalib:
+            import cv2
+            from cv2 import cv
+            # nova kalibrace pomoci opencv
+            if self.actualPointInd == self.nPoints:
             
-        if self.actualPointInd == 4:
-            #self.stav = 'sledovani'       
-             
-            #print self.projPts
-            fp = np.array([[self.projPts[0][0],self.projPts[1][0],self.projPts[2][0 ],self.projPts[3][0]],[self.projPts[0][1],self.projPts[1][1],self.projPts[2][1],self.projPts[3][1]],[1,1,1,1]])
-            tp = np.array([[self.kinectPts[0][0],self.kinectPts[1][0],self.kinectPts[2][0],self.kinectPts[3][0]],[self.kinectPts[0][1],self.kinectPts[1][1],self.kinectPts[2][1],self.kinectPts[3][1]],[1,1,1,1]])
-            self.H=Haffine_from_points(fp, tp)
-            print fp
-            print self.H
+                obj_points = np.array(self.kinectPts,'float32')
+                img_points = np.array(self.projPts,'float32')
             
-            with open('matice_kal2', 'wb') as f:
-                pickle.dump(self.H,f)
+                #w = 1680
+                #h = 1050
+                size = (self.width,self.height)
+            
+                w = 1680
+                h = 1050
+                size = (w,h)
+                camera_matrix = np.zeros((3, 3),'float32')
+                camera_matrix[0,0]= 2200.0
+                camera_matrix[1,1]= 2200.0
+                camera_matrix[2,2]=1.0
+                camera_matrix[0,2]=750.0
+                camera_matrix[1,2]=750.0 
+            
+                dist_coefs = np.zeros(4,'float32')
+                
+                #retval,camera_matrix,dist_coefs,rvecs,tvecs
+                print obj_points
+                print img_points
+                print size
+                retval,camera_matrix,dist_coefs,rvecs,tvecs = cv2.calibrateCamera([obj_points],[img_points],size,camera_matrix,dist_coefs,flags=cv.CV_CALIB_USE_INTRINSIC_GUESS)
+                
+                kalib_params = (retval,camera_matrix,dist_coefs,rvecs,tvecs)
+                with open('matice_kal', 'wb') as f:
+                    pickle.dump(kalib_params,f)
+                print "Kalibrace dokoncena"    
+            pass
+        else:
+            
+                
+            
+                
+            if self.actualPointInd == 4:
+                #self.stav = 'sledovani'       
+                 
+                #print self.projPts
+                fp = np.array([[self.projPts[0][0],self.projPts[1][0],self.projPts[2][0 ],self.projPts[3][0]],[self.projPts[0][1],self.projPts[1][1],self.projPts[2][1],self.projPts[3][1]],[1,1,1,1]])
+                tp = np.array([[self.kinectPts[0][0],self.kinectPts[1][0],self.kinectPts[2][0],self.kinectPts[3][0]],[self.kinectPts[0][1],self.kinectPts[1][1],self.kinectPts[2][1],self.kinectPts[3][1]],[1,1,1,1]])
+                self.H=Haffine_from_points(fp, tp)
+                print fp
+                print self.H
+                
+                with open('matice_kal2', 'wb') as f:
+                    pickle.dump(self.H,f)
+                print "Kalibrace dokoncena" 
                 
                 
-def projekce(point, kalib_params, new_kalib = True):
+def projekce(point, kalib_params, new_kalib = False):
+    
     if new_kalib:
-        pass
+        import cv2
+        retval,camera_matrix,dist_coefs,rvecs,tvecs = kalib_params
+        #point = [[280,-23.0,641]]
+        point_np = np.array([point], dtype = np.float64)
+        print point_np.dtype
+        print point_np
+        
+        ip, jacob = cv2.projectPoints(point_np, 
+            np.array(rvecs), 
+            np.array(tvecs), 
+            np.array(camera_matrix), 
+            np.array(dist_coefs))
+        
+        proj_point = ip[0,0]
     else:
+        point[2]=1
         proj_point = np.dot(kalib_params, point)
         
     return proj_point
