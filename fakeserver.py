21b5d9527d17ceb97380e71a5d698eb7ebc68d90
mkubat <majkon25@gmail.com>
2013-11-20 19:42:37 +0100

    Falešný server, který dodává námi zadané body.

--- fakeserver.py
+++ fakeserver.py
@@ -11,7 +11,7 @@ Window_height = 700
 Window_width = 1300                               
 
 
-# body = {'Torso':{'X':300,'Y':50,'Z':50},
+#  body = {'Torso':{'X':300,'Y':50,'Z':50},
 #         'Neck':{'X':100,'Y':100,'Z':50},
 #         'Head':{'X':400,'Y':50,'Z':100}}
 body = {'Torso':{'X':(Window_width/2),'Y':(Window_height-300),'Z':50},
